10938c1056d2dfcc5ccaa8080f12439e506dbae5
sername=rob <sername=rob@localhost>
2009-12-29 15:26:33 +0000

    Messages implemented

--- python/TCP/qClient.py
+++ python/TCP/qClient.py
@@ -22,24 +22,55 @@ class ClientGUI(QtGui.QWidget):
         self.connButton.clicked.connect(self.serverConnect)
         self.vLayout.addWidget(self.connButton)
         
+        self.msgText=QtGui.QLineEdit('Client Msg')
+        self.vLayout.addWidget(self.msgText)
+        
         self.sendButton=QtGui.QPushButton('Send')
-        self.sendButton.clicked.connect(self.sendMsg)
+        self.sendButton.clicked.connect(self.sendUserMsg)
+        self.sendButton.setEnabled(False)
         self.vLayout.addWidget(self.sendButton)
         
     def serverConnect(self):    
+        self.connButton.setText('Connecting...')
+        self.connButton.setEnabled(False)
         self.tcpSocket=QtNetwork.QTcpSocket(self)
         self.tcpSocket.readyRead.connect(self.readStream)
         self.tcpSocket.connectToHost(self.specAddr.text(), self.specPort.text().toInt()[0])
+        if not self.tcpSocket.waitForConnected(5000):
+            print 'Failed to connect, error code:', self.tcpSocket.error()
+            self.tcpSocket.close()
+            self.close()
+        self.connButton.setText('Connected')
+        self.sendButton.setEnabled(True)
                        
     def readStream(self):
         data=self.tcpSocket.readLine()
-        print data
+        print "Received: ", data
         
-    def sendMsg(self):
-        data="Message"
-        self.tcpSocket.writeData(data)
+        msgs=data.split(';')
+        
+        for msg in msgs:
+            if not msg=='':
+                print msg
+                sender, msgt, args = msg.split(',')
+                if sender=='server' and msgt=='new':
+                    self.clientID=args
+                    self.setWindowTitle("QClient: %s" % self.clientID)
+                if sender=='server' and msgt=='kill':
+                    self.sendMsg('dead','')
+                    self.tcpSocket.close()
+                    self.close()
+                else:
+                    pass
+    
+    def sendUserMsg(self):
+        self.sendMsg('user',self.msgText.text())
+            
+    def sendMsg(self,msg,args):
+        message=self.clientID+','+msg+','+args
+        self.tcpSocket.writeData(message)
         self.tcpSocket.waitForBytesWritten()
-        print "Written: ", data 
+        print "Written: ", msg, args 
         
 if __name__ == "__main__":
     app = QtGui.QApplication(argv)
