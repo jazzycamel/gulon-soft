82ce926d9b388049ca3ea669c3bd5c3ed4b05c4e
rob <rob@localhost>
2010-05-25 17:26:18 +0100

    New UDP test programs added

---
+++ python/UDP/udp_r.py
@@ -0,0 +1,35 @@
+from sys import argv
+
+#from QtCore import
+from PyQt4.QtNetwork import QUdpSocket
+from PyQt4.QtGui import QApplication, QWidget, QTextEdit, QVBoxLayout
+
+class UdpRx(QWidget):
+    def __init__(self, parent=None):
+        QWidget.__init__(self, parent)
+
+        self.setWindowTitle("UDP Receive")
+        self.setGeometry(300,300,300,300)
+
+        self.us=QUdpSocket(self)
+        self.us.bind(5000)
+        self.us.readyRead.connect(self.udpRead)
+
+        self.trace=QTextEdit()
+        self.trace.setReadOnly(True)
+        self.trace.append("UDP Trace:")
+
+        l=QVBoxLayout()
+        l.addWidget(self.trace)
+
+        self.setLayout(l)
+
+    def udpRead(self):
+        while self.us.hasPendingDatagrams():
+            datagram, host, port=self.us.readDatagram(self.us.pendingDatagramSize())
+            self.trace.append("%s:%s -> %s" % (host.toString(), port, datagram))
+
+app=QApplication(argv)
+u=UdpRx()
+u.show()
+app.exec_()
