7f4625efff02e785ca87cad3cc2be1ddd272ce9b
Rob <Rob@localhost>
2012-01-10 20:32:08 +0000

    Further changes to TCP

---
+++ python/TCP/client.py
@@ -0,0 +1,39 @@
+from sys import argv, exit
+from functools import wraps
+from PyQt4.QtGui import QApplication, QWidget
+from PyQt4.QtNetwork import QTcpSocket, QHostAddress
+
+def message(method):
+    message.messages.add(method.__name__)
+    @wraps(method)
+    def decorated(self, sender, *args, **kwargs):
+        method(self, sender, *args, **kwargs)
+    return decorated
+message.messages=set()
+
+class Client(QWidget):
+    def __init__(self, parent=None):
+        QWidget.__init__(self)
+
+        self.socket=_s=QTcpSocket()
+        _s.connected.connect(self._connected)
+        _s.connectToHost(QHostAddress(argv[1]), int(argv[2]))
+        if not _s.waitForConnected(5000):
+            print "Failed!"
+            _s.close()
+
+    def _connected(self):
+        self._write("Hello World")
+
+    def _write(self, d):
+        self.socket.write(d)
+        while self.socket.waitForBytesWritten(): continue
+
+if __name__=="__main__":
+    if len(argv)<3: 
+        print "Usage: python client.py <HOST> <PORT>"
+        exit()
+    a=QApplication(argv)
+    c=Client()
+    c.show()
+    exit(a.exec_())
